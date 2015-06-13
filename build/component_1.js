60cf90f8946707ed14a214065362d3cd94a3ac93
Warren <warren.kushner@gmail.com>
2015-06-13 14:56:23 -0400

    set state example

--- build/component_1.js
+++ build/component_1.js
@@ -1,13 +1,75 @@
-var MessageComponent = React.createClass({displayName: "MessageComponent",
+var Board = React.createClass({displayName: "Board",
   render: function() {
+    var className = "board";
+    if (this.props.selected) {
+      className += " selected";
+    }
     return (
-      React.createElement("div", null, this.props.message)
+      React.createElement("div", {className: className}, 
+        this.props.index + 1
+      )
     );
   }
 });
 
-// Render an instance of MessageComponent into document.body
+
+
+
+var BoardSwitcher = React.createClass({displayName: "BoardSwitcher",
+  
+  getInitialState: function(){
+    var boardArray=[];
+    for(var i=0; i<this.props.numBoards;i++){
+     
+     i===0 ? boardArray.push({isSelected:true}) : boardArray.push({isSelected:false})
+
+    }
+    //alert(boardArray);
+    return{boards: boardArray };
+
+  },
+  toggle: function(){
+    var boards = this.state.boards;
+   // var boardArray=this.state.boards];
+   console.log('hello');
+    for(var i=0; i<boards.length; i++){
+      if(boards[i].isSelected===true && boards.length-1===i){
+        boards[i].isSelected=false;
+        boards[0].isSelected = true;
+        console.log(JSON.stringify(boards));
+        break;
+      }
+
+      else if(boards[i].isSelected===true ){
+        boards[i].isSelected=false;
+        boards[i+1].isSelected = true;
+        console.log(JSON.stringify(boards));
+        break;
+      }
+      
+    }
+       this.setState({boards:boards});
+  },
+  render: function() {
+    var boards = [];
+    for (var ii = 0; ii < this.state.boards.length; ii++) {
+     // var isSelected = ii === 0;
+     isSelected = this.state.boards[ii].isSelected;
+      boards.push(
+        React.createElement(Board, {index: ii, selected: isSelected, key: ii})
+      );
+    }
+    
+    return (
+      React.createElement("div", null, 
+        React.createElement("div", {className: "boards"}, boards), 
+        React.createElement("button", {onClick: this.toggle}, "Toggle")
+      )
+    );
+  }
+});
+
 React.render(
-  React.createElement(MessageComponent, {message: "Hello!"}),
+  React.createElement(BoardSwitcher, {numBoards: 3}),
   document.body
 );
--- javascript/component_1.js
+++ javascript/component_1.js
@@ -1,13 +1,75 @@
-var MessageComponent = React.createClass({
+var Board = React.createClass({
   render: function() {
+    var className = "board";
+    if (this.props.selected) {
+      className += " selected";
+    }
     return (
-      <div>{this.props.message}</div>
+      <div className={className}>
+        {this.props.index + 1}
+      </div>
     );
   }
 });
 
-// Render an instance of MessageComponent into document.body
+
+
+
+var BoardSwitcher = React.createClass({
+  
+  getInitialState: function(){
+    var boardArray=[];
+    for(var i=0; i<this.props.numBoards;i++){
+     
+     i===0 ? boardArray.push({isSelected:true}) : boardArray.push({isSelected:false})
+
+    }
+    //alert(boardArray);
+    return{boards: boardArray };
+
+  },
+  toggle: function(){
+    var boards = this.state.boards;
+   // var boardArray=this.state.boards];
+   console.log('hello');
+    for(var i=0; i<boards.length; i++){
+      if(boards[i].isSelected===true && boards.length-1===i){
+        boards[i].isSelected=false;
+        boards[0].isSelected = true;
+        console.log(JSON.stringify(boards));
+        break;
+      }
+
+      else if(boards[i].isSelected===true ){
+        boards[i].isSelected=false;
+        boards[i+1].isSelected = true;
+        console.log(JSON.stringify(boards));
+        break;
+      }
+      
+    }
+       this.setState({boards:boards});
+  },
+  render: function() {
+    var boards = [];
+    for (var ii = 0; ii < this.state.boards.length; ii++) {
+     // var isSelected = ii === 0;
+     isSelected = this.state.boards[ii].isSelected;
+      boards.push(
+        <Board index={ii} selected={isSelected} key={ii} />
+      );
+    }
+    
+    return (
+      <div>
+        <div className="boards">{boards}</div>
+        <button onClick={this.toggle}>Toggle</button>
+      </div>
+    );
+  }
+});
+
 React.render(
-  <MessageComponent message="Hello!" />,
+  <BoardSwitcher numBoards={3} />,
   document.body
 );
